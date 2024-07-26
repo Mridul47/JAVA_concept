d1e836630c19d1f12c5c967f87721969b073cc36
Mridul Koirala <mrkoirala58@gmail.com>
2024-07-26 18:49:03 +0545

    FirstJavaProgram

---
+++ FirstJavaProgram.java
@@ -0,0 +1,10 @@
+
+public class FirstJavaProgram {
+    //This is the first Java program as single line comment example
+
+    /* Here you see the example of multiline comments.
+    This will print "Hello, World!" as output */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
