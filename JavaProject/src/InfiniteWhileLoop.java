949320dc6800fdb925d12321ca458d63883beb41
Mridul Koirala <mrkoirala58@gmail.com>
2024-07-29 21:42:09 +0545

    WhileLoop.java and InfinitivWhileLoop.java done

---
+++ JavaProject/src/InfiniteWhileLoop.java
@@ -0,0 +1,12 @@
+public class InfiniteWhileLoop {
+    public static void main(String[] args) {
+        
+        while(true){
+            System.out.println("Infinitive loop");
+            
+        }
+    }
+    
+}
+
+/*This generates infinity loop type "ctrl+C" to end the loop */
---
+++ JavaProject/src/WhileLoop.java
@@ -0,0 +1,10 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i =2; //initialization
+        while(i<=10){ //condition)
+            System.out.println(i);
+            i++; //increament or decrement
+        }
+    }
+    
+}
